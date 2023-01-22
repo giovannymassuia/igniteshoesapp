99c0eda98ea1a64f0aefb818ec5e724bfcb65b38
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-01-22 15:28:05 -0800

    one signal config (android)

--- App.tsx
+++ App.tsx
@@ -1,13 +1,22 @@
-import { StatusBar } from 'react-native';
-import { NativeBaseProvider } from 'native-base';
-import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+import { StatusBar } from "react-native";
+import { NativeBaseProvider } from "native-base";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+import OneSignal from "react-native-onesignal";
+import { Routes } from "./src/routes";
 
-import { Routes } from './src/routes';
+import { THEME } from "./src/theme";
+import { Loading } from "./src/components/Loading";
 
-import { THEME } from './src/theme';
-import { Loading } from './src/components/Loading';
+import { CartContextProvider } from "./src/contexts/CartContext";
 
-import { CartContextProvider } from './src/contexts/CartContext';
+// get from env file
+const ONESIGNAL_APP_ID_ENV = process.env.ONE_SIGNAL_APP_ID!;
+console.log("ONESIGNAL_APP_ID_ENV", ONESIGNAL_APP_ID_ENV);
+OneSignal.setAppId(ONESIGNAL_APP_ID_ENV);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
