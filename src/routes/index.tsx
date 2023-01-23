4f404a93ab12dec4f2045eadf99298b1678a97fb
Giovanny Massuia <giovannycmassuia@gmail.com>
2023-01-22 17:01:32 -0800

    deep link integration

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,6 +5,21 @@ import { AppRoutes } from "./app.routes";
 import { Notification } from "../components/Notification";
 import { useEffect, useState } from "react";
 import OneSignal, { OSNotification } from "react-native-onesignal";
+import * as Linking from "expo-linking";
+
+const linking = {
+  prefixes: ["igniteshoesapp://", "com.giovannymassuia.igniteshoesapp://"],
+  config: {
+    screens: {
+      details: {
+        path: "details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+};
 
 export function Routes() {
   const { colors } = useTheme();
@@ -25,7 +40,7 @@ export function Routes() {
   theme.colors.background = colors.gray[700];
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
 
       {notification?.title && (
